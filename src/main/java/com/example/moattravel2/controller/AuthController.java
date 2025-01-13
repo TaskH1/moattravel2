8156898d7a1657658248969fd7c8a7f0f87c4464
Tasuku hideshima <hidey@MacBook-Pro-3.local>
2025-01-13 16:02:18 +0900

    Create WebSecurityConfig class containing SecurityFileterChain() and PasswordEncoder() mthod

--- src/main/java/com/example/moattravel2/controller/AuthController.java
+++ src/main/java/com/example/moattravel2/controller/AuthController.java
@@ -1,5 +1,13 @@
 package com.example.moattravel2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AuthController {
+	@GetMapping("/login")
+	public String login() {
+		return "auth/login";
+	}
 
 }
---
+++ src/main/java/com/example/moattravel2/security/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.moattravel2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebSecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+					//URL permmitted for all users
+					.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/").permitAll()
+					//URL permmitted for only admin users
+					.requestMatchers("/admin/**").hasRole("ADMIN")
+					//any other URL needs to be logged in
+					.anyRequest().authenticated()
+			)
+			.formLogin((form) -> form
+					.loginPage("/login")
+					.loginProcessingUrl("/login")
+					.defaultSuccessUrl("/?loggedIn")
+					.failureUrl("/login?error")
+					.permitAll()	
+			)
+			.logout((logout) -> logout
+					.logoutSuccessUrl("/?loggedOut")
+					.permitAll()
+			);
+		return http.build();	
+	}
+	
+	// Returned value will be registered to DI container by @Bean 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
