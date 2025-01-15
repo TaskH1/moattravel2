a15dba78fcbb31758bc194f0be936b69eda3b10b
Tasuku hideshima <hidey@MacBook-Pro-3.local>
2025-01-15 14:15:36 +0900

    Update HouseRepository.java

--- src/main/java/com/example/moattravel2/repository/HouseRepository.java
+++ src/main/java/com/example/moattravel2/repository/HouseRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel2.entity.House;
 
+//JpaRepository is an interface provided by Spring Data JPA
 //JpaRepository<エンティティのクラス型、主キーのデータ型>
 public interface HouseRepository extends JpaRepository<House, Integer> {
 	//Page is a generic type provided by Spring Data
 	//and it contains the House entity data
 	public Page<House> findByNameLike(String keyword, Pageable pageable);
-	
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
 
 }
